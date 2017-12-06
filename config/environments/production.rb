665875e30596485eeae877f8547a8e1f9b18252e
ny_bigapplejr <ny_bigapplejr@yahoo.co.jp>
2017-12-06 13:23:18 +0000

    1206

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://13.115.154.252/' }
+  config.action_mailer.default_url_options = { host: 'fierce-castle-46725.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
  {
