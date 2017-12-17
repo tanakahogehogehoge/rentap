241a35c3060fc08397d5d5b7e4af57c05ea2ddbe
ny_bigapplejr <ny_bigapplejr@yahoo.co.jp>
2017-12-17 07:56:02 +0000

    1655

--- app/models/myp.rb
+++ app/models/myp.rb
@@ -3,6 +3,7 @@ class Myp < ActiveRecord::Base
   validates :uaddress, presence: true
   validates :ubankaccount, presence: true
   validates :ubankaccount, numericality: { only_integer: true, greater_than: 0 }
-  
+  validates :uemail, presence: true
+
   mount_uploader :avatar, AvatarUploader
 end
